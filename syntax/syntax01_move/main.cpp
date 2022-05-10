6933a8f411058819a2df2e10b0ceef4e8cc2f805
Jeffery Cheng <chengcheng0829@gmail.com>
2022-05-10 23:29:36 +0800

    add more usage in syntax01_move

--- syntax/syntax01_move/main.cpp
+++ syntax/syntax01_move/main.cpp
@@ -1,7 +1,11 @@
+#include <fmt/ranges.h>
+#include <future>
 #include <iostream>
+#include <thread>
 #include "common/common.hpp"
 
 using namespace std;
+using namespace fmt;
 using namespace common;
 
 class MyStr {
@@ -14,6 +18,18 @@ class MyStr {
   private:
     string str_;
 };
+
+void carefullyCase01() {
+    cout << Section("Carefully Usage 01") << endl;
+    vector<double> data;
+    double x{0};
+    for (size_t i = 0; i < 10; ++i) {
+        x = i * 2;
+        data.emplace_back(move(x));
+    }
+
+    cout << format("data = {}", data) << endl;
+}
 
 int main(int argc, char* argv[]) {
     {
@@ -42,6 +58,8 @@ int main(int argc, char* argv[]) {
         MyStr myStr("data");
         cout << "myStr: " << myStr.str() << endl;
     }
+
+    carefullyCase01();
 
     return 0;
 }
