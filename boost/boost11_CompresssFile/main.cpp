9e0caf0b6667c09c3f777ee9dea2a89c36074d5d
Jeffery Cheng <chengcheng0829@gmail.com>
2022-05-12 14:51:09 +0800

    add decompress example

--- boost/boost11_CompresssFile/main.cpp
+++ boost/boost11_CompresssFile/main.cpp
@@ -1,8 +1,11 @@
+#include <fmt/ranges.h>
+#include <boost/iostreams/copy.hpp>
 #include <boost/iostreams/filter/gzip.hpp>
 #include <boost/iostreams/filter/zlib.hpp>
 #include <boost/iostreams/filtering_stream.hpp>
 #include <fstream>
 #include <iostream>
+#include <vector>
 #include "common/common.hpp"
 
 using namespace std;
@@ -12,17 +15,39 @@ using namespace common;
 int main(int argc, const char* argv[]) {
     initLog(argc, argv);
 
-    fstream outFs("./data/out.gz", ios::out | ios::binary);
-    boost::iostreams::filtering_ostream outStream;
-    // outStream.push(boost::iostreams::zlib_compressor{});
-    outStream.push(boost::iostreams::gzip_compressor{});
-    outStream.push(outFs);
+    {
+        // compress
+        fstream fs("./data/out.gz", ios::out | ios::binary);
+        boost::iostreams::filtering_ostream stream;
+        // stream.push(boost::iostreams::zlib_compressor{});
+        stream.push(boost::iostreams::gzip_compressor{});
+        stream.push(fs);
 
-    outStream << "data" << endl;
+        // stream << "data" << endl;
+        vector<char> data = {1, 2, 3, 4, 5, 6, 78, 89};
+        stream.write(data.data(), data.size());
 
-    outStream.reset();
-    outFs.close();
+        stream.reset();
+        fs.close();
+    }
 
+    {
+        // decompress
+        fstream fs("./data/out.gz", ios::in | ios::binary);
+        boost::iostreams::filtering_istream stream;
+        stream.push(boost::iostreams::gzip_decompressor{});
+        stream.push(fs);
+
+        // stringstream ss;
+        // boost::iostreams::copy(stream, ss);
+        // LOG(INFO) << ss.str();
+        vector<unsigned char> raw;
+        raw.assign(istreambuf_iterator<char>{stream}, {});
+        LOG(INFO) << format("raw: {}", raw);
+
+        stream.reset();
+        fs.close();
+    }
     closeLog();
     return 0;
 }
